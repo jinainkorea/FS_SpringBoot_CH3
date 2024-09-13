3e20bcb503df73330b5f2a85a38c429d0f096af6
jinainkorea <kimjina1152@gmail.com>
2024-09-13 09:49:21 +0900

    사용자 패키지 생성, 회원가입 기능 생성

---
+++ src/main/java/com/mysite/sbb3/User/UserController.java
@@ -0,0 +1,35 @@
+package com.mysite.sbb3.User;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+  private final UserService userService;
+
+  @GetMapping("/signup")
+  public String signup(UserCreateForm userCreateForm) {
+    return "signup_form";
+  }
+
+  @PostMapping("/signup")
+  public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+    if(bindingResult.hasErrors()) {
+      return "signup_form";
+    }
+
+    if(!userCreateForm.getPassword().equals(userCreateForm.getPassword_check())) {
+      bindingResult.rejectValue("password_check", "passwordInCorrect", "두개의 비밀번호가 일치하지 않습니다.");
+    }
+
+    userService.signup(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword());
+    return "redirect:/";
+  }
+}
