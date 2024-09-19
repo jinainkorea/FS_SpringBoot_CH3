a664ff3ab804e790e4a0bea1dcd7177cae94beb5
jinainkorea <kimjina1152@gmail.com>
2024-09-19 10:09:07 +0900

    상세 페이지

--- src/main/java/com/std/basic1/Article/ArticleController.java
+++ src/main/java/com/std/basic1/Article/ArticleController.java
@@ -6,17 +6,26 @@ import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Getter
 @Setter
 @RequiredArgsConstructor
+@RequestMapping("article")
 public class ArticleController {
     private final ArticleService articleService;
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("articleList", this.articleService.getAll());
         return "article_list";
+    }
+
+    @GetMapping(value="/detail/{id}")
+    public String detail(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("article", this.articleService.getArticle(id));
+        return "article_detail";
     }
 }
--- src/main/java/com/std/basic1/Article/ArticleService.java
+++ src/main/java/com/std/basic1/Article/ArticleService.java
@@ -1,9 +1,11 @@
 package com.std.basic1.Article;
 
+import com.std.basic1.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,14 @@ public class ArticleService {
 
     public List<Article> getAll() {
         return this.articleRepository.findAll();
+    }
+
+    public Article getArticle(Integer id) {
+        Optional<Article> article = this.articleRepository.findById(id);
+        if (article.isPresent()) {
+            return article.get();
+        } else {
+            throw new DataNotFoundException("Article with id " + id + " not found");
+        }
     }
 }
