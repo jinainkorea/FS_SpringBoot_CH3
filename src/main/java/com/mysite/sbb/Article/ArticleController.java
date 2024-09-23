46bb847615fa326df9968c27be36df1fa313d46b
jinainkorea <kimjina1152@gmail.com>
2024-09-23 09:35:41 +0900

    글 생성 기능

--- src/main/java/com/mysite/sbb/Article/ArticleController.java
+++ src/main/java/com/mysite/sbb/Article/ArticleController.java
@@ -3,7 +3,9 @@ package com.mysite.sbb.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +18,19 @@ public class ArticleController {
   public String list(Model model) {
     model.addAttribute("articleList", this.articleService.list());
     return "article_list";
+  }
+
+  @GetMapping("/create")
+  public String create(ArticleForm articleForm) {
+    return "article_form";
+  }
+
+  @PostMapping("/create")
+  public String create(ArticleForm articleForm, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return "article_form";
+    }
+    this.articleService.createArticle(articleForm.getTitle(), articleForm.getContent());
+    return "redirect:/";
   }
 }
--- src/main/java/com/mysite/sbb/Article/ArticleService.java
+++ src/main/java/com/mysite/sbb/Article/ArticleService.java
@@ -3,7 +3,9 @@ package com.mysite.sbb.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,13 @@ public class ArticleService {
 
   public List<Article> list() {
     return this.articleRepository.findAll();
+  }
+
+  public void createArticle(String title, String content) {
+    Article article = new Article();
+    article.setTitle(title);
+    article.setContent(content);
+    article.setCreateDate(LocalDateTime.now());
+    this.articleRepository.save(article);
   }
 }
