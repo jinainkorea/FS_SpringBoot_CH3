10f84abb9fc5e696ea7a963b9db5cda16e2a26c5
jinainkorea <kimjina1152@gmail.com>
2024-09-23 09:18:36 +0900

    URL 루트, Article 엔티티 생성

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+  @GetMapping("/index")
+  @ResponseBody
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping("/")
+  public String root() {return "redirect: /article/list";}
+}
